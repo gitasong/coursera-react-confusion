7cc11d29d9d78a1f459032d840c2036bb887d42f
Nicole Freed <nicole.freed@gmail.com>
2019-02-06 10:21:13 -0800

    render media list

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -49,6 +49,13 @@ class Menu extends Component {
 
   render() {
     return(
+      <div className="container">
+        <div className="row">
+          <Media list>
+            {menu}
+          </Media>
+        </div>
+      </div>
     );
   }
 }
