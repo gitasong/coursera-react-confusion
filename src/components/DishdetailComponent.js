14ef7a971bc22df737e9537a61c7e4b26abccbaa
Nicole Freed <nicole.freed@gmail.com>
2019-02-12 18:28:33 -0800

    move dish detail card to DishDetail component

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -4,6 +4,21 @@ import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 class DishDetail extends Component {
 
   render() {
+    const { dish } = this.props;
+
+      return(
+        <div className="row">
+          <div className="dishDetail col-12 col-md-5 m-1">
+            <Card dish={dish}>
+              <CardImg width="100%" src={dish.image} alt={dish.name} />
+              <CardBody>
+                <CardTitle>{dish.name}</CardTitle>
+                <CardText>{dish.description}</CardText>
+              </CardBody>
+            </Card>
+          </div>
+        </div>
+      );
   }
 }
 
